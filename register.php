94b2bd9029a3dd05e59b2d44fb42a5806bf71877
Sisekelo <sidlamini15@alustudent.com>
2018-01-23 13:56:57 +0400

    a

--- register.php
+++ register.php
@@ -30,10 +30,17 @@ $room = $mysqli->escape_string($_POST['room']);
 $referer = $mysqli->escape_string($_POST['referer']);
 $residence = $mysqli->escape_string($_POST['residence']);
 
-/*if (strpos($email, 'alustudent') === false) {
-    $_SESSION['message'] = 'We are open to ALU students only for now';
+if (strpos($email, 'alustudent') === false) {
+    $sql = "INSERT INTO nonALU (name,surname,email,number,picture,RoomNumber,Referer)
+            VALUES ('$first_name','$last_name','$email','$number','$picURL','$room $residence','$referer')";
+    $mysqli->query($sql);
+
+    $_SESSION['emoji'] = "&#x1F625;";
+    $_SESSION['message'] = 'Thank you for the enthusiasm but Oui Deliver is only open for ALU students.';
     header("location: notification.php"); 
-}*/
+}
+
+else{
 
       
 // Check if user with that email already exists
@@ -48,59 +55,59 @@ if ( $result->num_rows > 0 ) {
 }
 else { // Email doesnt already exist in a database, proceed...
 
-    // active is 0 by DEFAULT (no need to include it here
-     $sql = "INSERT INTO details (name,surname,email,number,picture,RoomNumber,Referer)
-            VALUES ('$first_name','$last_name','$email','$number','$picURL','$room $residence','$referer')";
+        // active is 0 by DEFAULT (no need to include it here
+         $sql = "INSERT INTO details (name,surname,email,number,picture,RoomNumber,Referer)
+                VALUES ('$first_name','$last_name','$email','$number','$picURL','$room $residence','$referer')";
 
-    // Add user to the database
-    if ( $mysqli->query($sql) ){
+        // Add user to the database
+        if ( $mysqli->query($sql) ){
 
-        $_SESSION['Active'] = 0; //0 until user activates their account with verify.php
-        $_SESSION['logged_in'] = true; // So we know the user has logged in
-        $_SESSION['message'] =
-                 "Confirmation link has been sent to $email, please verify
-                 your account by clicking on the link in the message! \n Check your spam to make sure email has arrived";
-                 $_SESSION['superURL'] = 'https://ouideliver.xyz/Foodie/verify.php?email='.$email;
+            $_SESSION['Active'] = 0; //0 until user activates their account with verify.php
+            $_SESSION['logged_in'] = true; // So we know the user has logged in
+            $_SESSION['message'] =
+                     "Confirmation link has been sent to $email, please verify
+                     your account by clicking on the link in the message! \n Check your spam to make sure email has arrived";
+                     $_SESSION['superURL'] = 'https://ouideliver.xyz/Foodie/verify.php?email='.$email;
 
-                 //send a text message
+                     //send a text message
 
-                  // Send registration confirmation link (verify.php)
+                      // Send registration confirmation link (verify.php)
 
-     $thanks = 'https://ouideliver.xyz/Foodie/verify.php?email='.$email;
-     /*$done = preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $thanks);
+         $thanks = 'https://ouideliver.xyz/Foodie/verify.php?email='.$email;
+         /*$done = preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $thanks);
 
-    include ( "Nexmo-PHP-lib-master/NexmoMessage.php" );  */
-    // Step 1: Declare new NexmoMessage.
-    /*$nexmo_sms = new NexmoMessage('d6726b9a', '005e2f3453ccb56c');*/
-    // Step 2: Use sendText( $to, $from, $message ) method to send a message. 
-    /*$info = $nexmo_sms->sendText( $number, 'MyApp',''.$thanks);*/
+        include ( "Nexmo-PHP-lib-master/NexmoMessage.php" );  */
+        // Step 1: Declare new NexmoMessage.
+        /*$nexmo_sms = new NexmoMessage('d6726b9a', '005e2f3453ccb56c');*/
+        // Step 2: Use sendText( $to, $from, $message ) method to send a message. 
+        /*$info = $nexmo_sms->sendText( $number, 'MyApp',''.$thanks);*/
 
 
-    //send email
+        //send email
 
-        $to   = $email;
-        $from = 'sidlamini15@alustudent.com';
+            $to   = $email;
+            $from = 'sidlamini15@alustudent.com';
 
-        $subject = "Oui Deliver: Verify your account."; 
+            $subject = "Oui Deliver: Verify your account."; 
 
-        $headers = "From: " . $from . "\r\n";
-        $headers .= "Reply-To: ". $from . "\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+            $headers = "From: " . $from . "\r\n";
+            $headers .= "Reply-To: ". $from . "\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-        $message = '<html><body><br><br>';
-        $message .= '<h1 style="text-align: center; font-family: Avenir">Hi, Sisekelo. Welcome to Oui Deliver.</h1> <br>';
-        $message .= '<h2 style="text-align: center; font-family: Avenir">Let us verify your account.</h2><br>';
-        $message .= '<p style="text-align: center;font-family: Avenir;border: 1px solid blue;background: #04b4aa;padding: 5%;text-decoration: none;color: black;font-size: 150%;margin: 0;"><a href="'.$thanks.'" style="text-decoration: none; color: black;padding: 5%">Click here to verify account</a></p>"';
-        $message .= '</body></html>';
+            $message = '<html><body><br><br>';
+            $message .= '<h1 style="text-align: center; font-family: Avenir">Hi, Sisekelo. Welcome to Oui Deliver.</h1> <br>';
+            $message .= '<h2 style="text-align: center; font-family: Avenir">Let us verify your account.</h2><br>';
+            $message .= '<p style="text-align: center;font-family: Avenir;border: 1px solid blue;background: #04b4aa;padding: 5%;text-decoration: none;color: black;font-size: 150%;margin: 0;"><a href="'.$thanks.'" style="text-decoration: none; color: black;padding: 5%">Click here to verify account</a></p>"';
+            $message .= '</body></html>';
 
-        mail($to, $subject, $message, $headers);
+            mail($to, $subject, $message, $headers);
 
-    header("location: profile.php");
-    }
-    else {
-        $_SESSION['emoji'] = "&#x1F625;";
-        $_SESSION['message'] = 'Registration failed!';
-       header("location: error.php");
-    }
-}
+        header("location: profile.php");
+        }
+        else {
+            $_SESSION['emoji'] = "&#x1F625;";
+            $_SESSION['message'] = 'Registration failed!';
+           header("location: error.php");
+        }
+        }
